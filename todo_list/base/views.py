3716ef6d3ec1f2781a8e841982e476df29e25abe
alianwar3 <74695208+alianwar3@users.noreply.github.com>
2022-09-01 15:33:01 -0400

    Update import for "Task" in view.py

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-
 from .models import Task
 
 class CustomLoginView(LoginView):
