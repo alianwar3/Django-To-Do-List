1422dc7d281588dfa67bddb0979c24b1dfa160ae
alianwar3 <74695208+alianwar3@users.noreply.github.com>
2022-09-04 10:38:53 -0400

    Updated url.py to include comment for URL's with CRUD operations.

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', RegisterPage.as_view(), name='register'),
 
+    # URL path for task CRUD operations
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
